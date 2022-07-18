b8163a502b462c9b91a179c72b189e0a2f144a3f
Garhett <60421166+garhett@users.noreply.github.com>
2022-07-18 02:59:49 +0000

    Update

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int one = 1;
+		int two = 2;
+		int three = 3;
+		int four = 4;
+		int five = 5;
+		int six = 6;
+		int seven = 7;
+		int eight = 8;
+		int nine = 9;
+		int ten = 10;
+
+		int[] numArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
   }
 }
