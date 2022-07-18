ea98432493528e89f05a3a012689837944c7ab33
Garhett <andersongarhett@yahoo.com>
2022-07-17 22:25:21 -0600

    Update

--- Main.java
+++ Main.java
@@ -27,9 +27,12 @@ With that number, see who makes the best cookie from the list below:
 https://www.linkedin.com/posts/peteralarkin_cookiewars-activity-6954525462115057664---UJ?utm_source=linkedin_share&utm_medium=member_desktop_web
  */
 
+import java.util.ArrayList;
+
 class Main {
     public static void main(String[] args) {
         int[] numArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        ArrayList<Integer> numlist = new ArrayList<>();
 
         for (int i = 0; i < numArray.length; i++) {
             int tempNum = numArray[i];
@@ -38,10 +41,21 @@ class Main {
             numArray[i] *= 3; // multiply by 3 again
             int sum = sumOfDigits(numArray[i]); // add the digits together
             int tempFinalNum = numArray[i];
+            numlist.add(sum);
 
             System.out.println("Number " + tempNum + " equates to " + tempFinalNum
                     + ", which added together makes " + sum + ". This equals " + numToCookie(sum) + ".");
         }
+
+        if (doesAllMatch(numlist)) {
+            System.out.println("\nClick here: https://i.kym-cdn.com/entries/icons/facebook/000/000/157/itsatrap.jpg");
+        }
+    }
+
+    static boolean doesAllMatch(ArrayList<Integer> ArrayList) {
+        return ArrayList.stream()
+                .distinct()
+                .count() <= 1;
     }
 
     static int sumOfDigits(int num) {
